d703bb7e814c79986a05748bd0506ee443ed6d24
Ahmad6700 <intern.alphasquad@gmail.com>
2021-10-06 01:31:57 +0500

    first-Commit

---
+++ code.js
@@ -0,0 +1,23 @@
+figma.showUI(__html__, { width: 648, height: 650, title: 'Figma to Tailwind' });
+figma.ui.onmessage = msg => {
+    if (msg.category === 'breakpoints') {
+        let key = msg.nodeID;
+        let value = msg.classes;
+        console.log(key, value);
+    }
+    else if (msg.category === 'customClasses') {
+        let key = msg.nodeID;
+        let value = msg.customClasses;
+        console.log(key, value);
+    }
+    else if (msg.category === 'customInteractions') {
+        let key = msg.nodeID;
+        let value = msg.customInteractions;
+        console.log(key, value);
+    }
+    else if (msg.category === 'tagName') {
+        let key = msg.nodeID;
+        let value = msg.tagName;
+        console.log(key, value);
+    }
+};
